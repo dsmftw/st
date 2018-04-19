335f9d758f79f387290505e92c43d987c831b385
Igor Kozlov <ikozlov11@gmail.com>
2018-04-18 18:04:28 -0700

    add correct font settings string

--- config.def.h
+++ config.def.h
@@ -5,7 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char font[] = "Hack:pixelsize=14:antialias=true:autohint=true";
+//static char font[] = "DejaVu Sans Mono:pixelsize=14:antialias=true:autohint=true";
+//static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
