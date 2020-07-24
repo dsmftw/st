189b8e3c6f714316885574fe7218cdf1d8d2bc02
dsmftw <ikozlov11@gmail.com>
2020-07-23 19:21:47 -0700

    rebase to new ver upstream and clean up font

--- config.def.h
+++ config.def.h
@@ -5,9 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "Hack:pixelsize=14:antialias=true:autohint=true";
-//static char font[] = "DejaVu Sans Mono:pixelsize=14:antialias=true:autohint=true";
-//static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Hack:pixelsize=11:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
